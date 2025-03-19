f50851fec5eaf377a45ae73f223d8b32c4d0b708
salo <salomontilla@outlook.com>
2025-03-19 14:36:53 -0500

    agregar clase producto con sus metodos y atributos

---
+++ app/src/main/java/com/salomon/ejercicios_clase_9/models/Producto.java
@@ -0,0 +1,47 @@
+package com.salomon.ejercicios_clase_9.models;
+
+public class Producto {
+    private int id;
+    private String nombre;
+    private double precio;
+    private Categoria categoria;
+
+    public Producto(int id, String nombre, double precio, Categoria categoria) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.categoria = categoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
