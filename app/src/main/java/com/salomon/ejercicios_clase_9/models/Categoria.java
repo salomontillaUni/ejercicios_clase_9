3a48a8a9f04e3e437f0961f727bd60229486ac9a
salo <salomontilla@outlook.com>
2025-03-19 14:38:46 -0500

    crear clase categoria con metodos y atributos

---
+++ app/src/main/java/com/salomon/ejercicios_clase_9/models/Categoria.java
@@ -0,0 +1,17 @@
+package com.salomon.ejercicios_clase_9.models;
+
+public class Categoria {
+    private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Categoria(String nombre) {
+        this.nombre = nombre;
+    }
+}
