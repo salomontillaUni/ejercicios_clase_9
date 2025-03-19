621fdb93939e1b029ad38b740d5ef2e97bc61d98
salo <salomontilla@outlook.com>
2025-03-19 14:47:12 -0500

    instanciar clases y probrar metodos

--- app/src/main/java/com/salomon/ejercicios_clase_9/MainActivity.java
+++ app/src/main/java/com/salomon/ejercicios_clase_9/MainActivity.java
@@ -8,6 +8,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.salomon.ejercicios_clase_9.models.Categoria;
+import com.salomon.ejercicios_clase_9.models.Inventario;
+import com.salomon.ejercicios_clase_9.models.Producto;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -20,5 +24,12 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        inventario.agregarProducto(p);
+        inventario.verProductos();
     }
+
+    Producto p = new Producto(0, "Iphone", 500.00,
+            new Categoria("Tecnologia"));
+    Inventario inventario = new Inventario();
+
 }
